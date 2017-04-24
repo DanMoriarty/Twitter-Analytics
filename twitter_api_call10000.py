d26e3d293af5599673fd92526df2f410601a06d5
Angus <foodusugna@gmail.com>
2017-04-24 12:56:14 +1000

    Lil fixes

--- twitter_api_call10000.py
+++ twitter_api_call10000.py
@@ -32,7 +32,6 @@ def insert_tweet(tweet):
             db[tweet['id_str']] = entry
 
 last_id = 2
-last_id_t = 2
 loops = 10000
 for i in range(loops):
     try:
@@ -43,13 +42,15 @@ for i in range(loops):
         # print last_id
         for tweet in results['statuses']:
             insert_tweet(tweet)
-            for j in range(100):
-                timeline = twitterAPI.user_timeline(id=tweet['user']['id'], count=200, max_id=str(last_id_t-1))
-                if not timeline:
-                    break
-                last_id_t = timeline[-1]['id']
-                for timeline_tweet in timeline:
-                    insert_tweet(timeline_tweet)
+            if tweet['coordinates']:
+                last_id_t = 2
+                for j in range(16):
+                    timeline = twitterAPI.user_timeline(id=tweet['user']['id'], count=200, max_id=str(last_id_t-1))
+                    if not timeline:
+                        break
+                    last_id_t = timeline[-1]['id']
+                    for timeline_tweet in timeline:
+                        insert_tweet(timeline_tweet)
     except:
         print 'Oops'
         break
