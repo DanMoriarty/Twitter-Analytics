1493f8060215976aeb532ed34da8994452bc576f
tglennan <tglennan@student.unimelb.edu.au>
2017-05-01 16:38:18 +1000

    Added an InfoList component to display dropdown lists of data (e.g. "Top x happiest suburbs") and frameworked the sentiment display

---
+++ web/src/material/InfoCard.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import {Card, CardHeader, CardText} from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
+import {List, ListItem} from 'material-ui/List';
+
+const styles = {
+  refresh: {
+    display: 'inline-block',
+    position: 'relative',
+  },
+  infoBox: {
+    width: '99%',
+    'margin-top': '5px',
+    'margin-bottom': '5px',
+  }
+};
+
+function InfoCardList(props) {
+  const listItems = props.items.map(
+        item => 
+        (
+          <ListItem
+            key={item.suburb}
+            primaryText={item.suburb}
+            secondaryText={item.data}
+            disabled={true}
+          />
+        ));
+  
+  return (
+    <List>
+      {listItems}
+    </List>
+  )
+};
+
+function InfoCard(props) {
+
+  return(
+    <Card style={styles.infoBox}>
+      <CardHeader
+        title={props.title}
+        subtitle={props.subtitle}
+        actAsExpander={true}
+        showExpandableButton={true}
+      />
+      <CardText expandable={true}>
+        <InfoCardList items={props.items} />
+      </CardText>
+    </Card>
+  )
+};
+
+export default InfoCard;
--- web/src/visualisation/Sentiment.js
+++ web/src/visualisation/Sentiment.js
@@ -1,13 +1,6 @@
 import React, {Component} from 'react';
 import RefreshIndicator from 'material-ui/RefreshIndicator';
-
-const style = {
-  refresh: {
-    display: 'inline-block',
-    position: 'relative',
-  },
-};
-
+import InfoCard from '../material/InfoCard.js'
 class Sentiment extends Component {
   
   constructor(props) {
@@ -20,22 +13,63 @@ class Sentiment extends Component {
   render() {
     if (!this.props.active) return null;
     
+    const placeholderData = 
+      [
+        {suburb:"Parkville", data: "20.00% Positive (Pos: 20, Neg: 80)"},
+        {suburb:"Melbourne", data: "98.03% Positive (Pos: 100, Neg: 2)"}
+      ]
+
     return (
-      <div className="center">
-        <RefreshIndicator
-          size={100}
-          left={0}
-          top={50}
-          loadingColor="#FF9800"
-          status="loading"
-          style={style.refresh}
-        />
+      <div className="container">
+        <div className="left">
+          +++++++++++++ GOOGLE MAPS GOES HERE +++++++++++++++ 
+          ++++++++++++++++ ++++++++++++++++
+          +++++++++++++ +++++++++++++++ ++++++++++++++++ ++++++++++++++++
+          +++++++++++++ +++++++++++++++ ++++++++++++++++ ++++++++++++++++
+          +++++++++++++ +++++++++++++++ ++++++++++++++++ ++++++++++++++++
+          <br/>+<br/>+<br/>+<br/>+<br/>+<br/>+<br/>+<br/>+<br/>+<br/>+
+          <br/>+<br/>+<br/>+<br/>+<br/>+<br/>+<br/>+<br/>+<br/>+<br/>+
+          <br/>+<br/>+<br/>+<br/>+<br/>+<br/>+<br/>+<br/>+<br/>+<br/>+
+        </div>
+        <div className="right">
+          <InfoCard
+            title="Happiest Suburbs"
+            subtitle="Highest ratio of positive tweets"
+            items={placeholderData}
+          />
+          <InfoCard
+            title="Unhappiest Suburbs"
+            subtitle="Highest ratio of negative tweets"
+            items={placeholderData}
+          />
+          <InfoCard
+            title="Neutral Suburbs"
+            subtitle="Highest ratio of neutral tweets"
+            items={placeholderData}
+          />
+          <InfoCard
+            title="Controversial Suburbs"
+            subtitle="Most mixed ratio of postive/negative tweets"
+            items={placeholderData}
+          />
+        </div>
       </div>
 
       );
   }
 }
 
+
+// <div className="center">
+//   <RefreshIndicator
+//     size={100}
+//     left={0}
+//     top={50}
+//     loadingColor="#FF9800"
+//     status="loading"
+//     style={styles.refresh}
+//   />
+// </div>
 
 
 export default Sentiment;
