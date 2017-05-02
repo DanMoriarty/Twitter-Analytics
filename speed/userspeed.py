d114ed4709651d13ffa358c259cac5d963fa90ac
Tom Lynch <tomglynch@Toms-MacBook-Pro.local>
2017-05-02 20:16:06 +1000

    updated these

--- speed/userspeed.py
+++ speed/userspeed.py
@@ -6,7 +6,7 @@ from datetime import datetime
 
 USER = "clustercloud"
 PSWD = "pineapple"
-HOST = "115.146.95.86" #Sherlock
+HOST = "127.0.0.1" #itself
 PORT = "8888"
 
 DB   = "melbtweets2"   #The main tweets database.
@@ -18,56 +18,63 @@ DB   = "melbtweets2"   #The main tweets database.
 
 def creating_user_docs(db):
 	x = 0
+	z = 0
 	for ID in db:
 		try:
-			user = db[ID]['user']['screen_name']
-			x = x + 1
-
-			if user in db:
-				doc = db[user]
-				
-				# HERE GOES THE CODE FOR ROUND 2 
-				# userspeed = {'type':'userspeeds','tweetid':[db[ID]['_id']],'speed':[0],'text':[db[ID]['text']],'created_at':[db[ID]['created_at']],'coordinates':[db[ID]['coordinates']['coordinates']]}
-				# speeddata = {'speed': '0','tweetid1':'0','tweetid2': ,'created_at1':'','created_at2': ,'coords1': ,'coords2': }
-				created_at1 = db[user]['userspeed'][-1]['created_at2']
-				created_at2 = db[ID]['created_at']
-				time1 = datetime.strptime(created_at1,'%a %b %d %H:%M:%S +0000 %Y')
-				time2 = datetime.strptime(created_at2,'%a %b %d %H:%M:%S +0000 %Y')
-				coords1 = db[user]['userspeed'][-1]['coords2']
-				coords2 = db[ID]['coordinates']['coordinates']
-
-				tweetid1 = db[user]['userspeed'][-1]['tweetid2']
-				tweetid2 = db[ID]['_id']
-
-				distance = haversine([coords1[1],coords1[0]],[coords2[1],coords2[0]])
-				time_h = ((time2 - time1).total_seconds())/60/60
-				speed = distance/time_h
-
-				if(speed > 25):
-					print("")
-					print speed, user, tweetid1, tweetid2
-
-
-				speeddata = {'speed': speed,'tweetid1':tweetid1,'tweetid2':tweetid2,
-				'created_at1':created_at1,'created_at2':created_at2,'coords1':coords1,'coords2':coords2}
-
-				doc['userspeed'].append(speeddata)
-				db[user] = doc
-			else:
-				# HERE GOES THE CODE FOR INITIALISING THE USER				
-				speeddata = {'tweetid2':db[ID]['_id'],'created_at2':db[ID]['created_at'] ,'coords2':db[ID]['coordinates']['coordinates']}
-				db[user] = {'userspeed':[speeddata]}
-				# db[user] = {'type':'user','tweetid':[db[ID]['_id']]}
-				
-			tweet = db[ID]
-			tweet['in_user_doc'] = 'true'
-			db[ID] = tweet
-
-			if(x == 100):
-				print("."),
-				x = 0
+			db[ID]['in_user_doc']
+			z = z + 1
+			if(z == 500):
+				print "*",
+				z = 0
 		except:
-			y = 0
+			try:
+				user = db[ID]['user']['screen_name']
+				x = x + 1
+
+				if user in db:
+					doc = db[user]
+					
+					# HERE GOES THE CODE FOR ROUND 2 
+					# userspeed = {'type':'userspeeds','tweetid':[db[ID]['_id']],'speed':[0],'text':[db[ID]['text']],'created_at':[db[ID]['created_at']],'coordinates':[db[ID]['coordinates']['coordinates']]}
+					# speeddata = {'speed': '0','tweetid1':'0','tweetid2': ,'created_at1':'','created_at2': ,'coords1': ,'coords2': }
+					created_at1 = db[user]['userspeed'][-1]['created_at2']
+					created_at2 = db[ID]['created_at']
+					time1 = datetime.strptime(created_at1,'%a %b %d %H:%M:%S +0000 %Y')
+					time2 = datetime.strptime(created_at2,'%a %b %d %H:%M:%S +0000 %Y')
+					coords1 = db[user]['userspeed'][-1]['coords2']
+					coords2 = db[ID]['coordinates']['coordinates']
+
+					tweetid1 = db[user]['userspeed'][-1]['tweetid2']
+					tweetid2 = db[ID]['_id']
+
+					distance = haversine([coords1[1],coords1[0]],[coords2[1],coords2[0]])
+					time_h = ((time2 - time1).total_seconds())/60/60
+					speed = distance/time_h
+
+					if(speed > 25):
+						print("")
+						print speed, user, tweetid1, tweetid2
+
+					speeddata = {'speed': speed,'tweetid1':tweetid1,'tweetid2':tweetid2,
+					'created_at1':created_at1,'created_at2':created_at2,'coords1':coords1,'coords2':coords2}
+
+					doc['userspeed'].append(speeddata)
+					db[user] = doc
+				else:
+					# HERE GOES THE CODE FOR INITIALISING THE USER				
+					speeddata = {'tweetid2':db[ID]['_id'],'created_at2':db[ID]['created_at'] ,'coords2':db[ID]['coordinates']['coordinates']}
+					db[user] = {'userspeed':[speeddata]}
+					# db[user] = {'type':'user','tweetid':[db[ID]['_id']]}
+					
+				tweet = db[ID]
+				tweet['in_user_doc'] = 'true-batch2'
+				db[ID] = tweet
+
+				if(x == 100):
+					print("."),
+					x = 0
+			except:
+				y = 0
 
 
 
