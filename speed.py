daf2b95c7ac24b831f2974200337d75a8d0f737f
Angus <foodusugna@gmail.com>
2017-05-03 21:01:11 +1000

    Ansible stuff to deploy instances and run couchdb

--- speed.py
+++ speed.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python
-#coding: utf-8
+# !/usr/bin/env python
+# coding: utf-8
 #####---------------------------- DESCRIPTION ----------------------------#####
 
-#    Authors:   T. Glennan, T. Lynch, D. Moriarty, S. Spratley, A. White
+#    Authors:   T. Glennan, D. Moriarty, T. Lynch, S. Spratley, A. White
 #    Course:    COMP90024 Cluster and Cloud Computing
 #    Project:   Melbourne Twitter analytics
 #    Purpose:   To process the database using separate analysis modules
@@ -11,28 +11,31 @@
 #####----------------------------   IMPORTS   ----------------------------#####
 
 import couchdb
-import sentiment
+# import sentiment
 
 #####----------------------------  CONSTANTS  ----------------------------#####
 
 USER = "clustercloud"
 PSWD = "pineapple"
-HOST = "115.146.95.86" #Sherlock
+HOST = "115.146.92.136" #Mario
 PORT = "8888"
 
-# DB   = "melbtweets2"   #The main tweets database.
-DB   = "melbtweets_sentiment" #100-tweet database for development.
+DB1   = "melbtweets_test1"   #The main tweets database.
+DB2   = "melbtweets2"
+# DB   = "melbtweets_sentiment" #100-tweet database for development.
 
 #####----------------------------  FUNCTIONS  ----------------------------#####
 
 #Function to read in tweets and assign sentiment labels to an entire database.
-def assign_sentiments(database):
-    for tweetID in database:
-        tweet = database[tweetID]
-        print tweet["doc"]["text"]
-        tweet["doc"]["sentiment"] = sentiment.classify(tweet["doc"]["text"])
-        print tweet["doc"]["sentiment"]
-        # database[tweetID] = tweet
+def assign_sentiments(database1, database2):
+    i = 0
+    for tweetID in database2:
+        i+=1
+        print i
+        tweet = database2[tweetID]
+        database1[tweetID] = tweet
+        if i == 100:
+            return
 
 #####----------------------------   PROGRAM   ----------------------------#####
 
@@ -40,6 +43,7 @@ def assign_sentiments(database):
 #Use following comment line to connect to server when credentials are required.
 #server = couchdb.Server("http://{0}:{1}@{2}:{3}".format(USER,PSWD,HOST,PORT))
 server = couchdb.Server("http://{0}:{1}".format(HOST,PORT))
-assign_sentiments(server[DB])
+print server
+assign_sentiments(server['melbtweets_test1'], server['melbtweets2'])
 
 #####----------------------------  END  FILE  ----------------------------#####
