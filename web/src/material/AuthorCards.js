94a04dedabca57cc29a20c3b3f74b62af4462f38
tglennan <tglennan@student.unimelb.edu.au>
2017-05-11 02:00:21 +1000

    Updated bio page, and updated package to allocate more memory to the node server

--- web/src/material/AuthorCards.js
+++ web/src/material/AuthorCards.js
@@ -16,37 +16,32 @@ function AuthorCards(props) {
     <div id="cards" style={style}>
       <AuthorCard
         name="Tim Glennan"
-        mainroles="NeCTAR / Sentiment / Node API / Web Design / Analytics"
+        mainroles="NeCTAR / Sentiment / Node API / Web Design / Maps / Analytics"
         avatar="tim.jpg"
-        studentid="587082"
         course="Master of Computer Science"
       />
       <AuthorCard
         name="Tom Lynch"
-        mainroles="NeCTAR / Tweet Scraping / Database Management / Analytics"
+        mainroles="NeCTAR / Tweet Harvesting / Database Management / Analytics"
         avatar="tom.jpg"
-        studentid="540621"
         course="Master of Software Engineering"
       />
       <AuthorCard
         name="Daniel Moriarty"
         mainroles="Maps Visualisation / Analytics"
         avatar="dan.jpg"
-        studentid="590617"
         course="Master of Mechanical Engineering"
       />
       <AuthorCard
         name="Steven Spratley"
-        mainroles="Sentiment / Language Modelling"
+        mainroles="Sentiment / Language Modelling / Analytics"
         avatar="steve.jpg"
-        studentid=""
         course="Master of Computer Science"
       />
       <AuthorCard
         name="Angus White"
-        mainroles="NeCTAR / Tweet Scraping / Database Management / Automation"
+        mainroles="NeCTAR / Tweet Harvesting / Database Management / Automation"
         avatar="angus.jpg"
-        studentid="585042"
         course="Master of Electrical Engineering"
       />
     </div>)
@@ -58,11 +53,11 @@ function AuthorCard(props) {
         <Card>
           <CardHeader
             title={props.name}
-            subtitle={props.mainroles}
+            subtitle={props.course}
             avatar={avatarPath + props.avatar}
           />
           <CardText>
-            {props.bio}
+            Key Roles: {props.mainroles}
           </CardText>
         </Card>
       </div>
