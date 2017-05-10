ffd500a58a26a8be4215c0ac8e4e48fd083663c3
Tom Lynch <tomglynch@Toms-MacBook-Pro.local>
2017-05-10 18:51:56 +1000

    route added for user tracking and movement map updated. it works.

--- restful/routes.js
+++ restful/routes.js
@@ -31,6 +31,9 @@ module.exports = function(router) {
 	router.get('/deviceSentiment/', controller.getDeviceSentiment);
 	router.get('/speed3k3m/', controller.getSpeed3k3m);
 	router.get('/speed3k150k3m/', controller.getSpeed3k150k3m);
+	router.get('/userLocations', controller.getUserLocations);
+	router.get('/userLocations/:screen_name', controller.getUserLocationsKey);
+
 
 	//--------------- PLAYING WITH KEYS ---------------//
 	// router middleware to handle key
@@ -43,6 +46,12 @@ module.exports = function(router) {
 	router.param('tweet', function(req, res, next, tweet) {
 		// update key
 		req.tweet = tweet.split("&");
+		next();
+	})
+
+	router.param('screen_name', function(req, res, next, screen_name) {
+		// update key
+		req.screen_name = screen_name.split("&popeye&hogscafe&");
 		next();
 	})
 
